5ed06a035adca84e13c4d6d6d97a5d619993e4af
Habiba Omer <habibaomer675@gmail.com>
2024-07-13 16:05:54 +0300

    add square funtion

---
+++ AreaAndPerimeter/AreaAndPerimeter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndPerimeter
+{
+    class Program
+    {
+        //1
+        public static void Square()
+        {
+            Console.WriteLine("Enter the sidelength of the square: ");
+            double sidelength = double.Parse(Console.ReadLine());
+            double area = sidelength * sidelength;
+            double perimeter = 4 * sidelength;
+            Console.WriteLine($"The Area Of Square = {area} And The perimeter = {perimeter}.");
+        }     
+        static void Main(string[] args)
+        {
+            Square();
+            Console.ReadLine();
+        }
+    }
+}

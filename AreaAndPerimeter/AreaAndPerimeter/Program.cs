b3d7715429c4e80c41042489c6bae4a7dca8ab98
Habiba Omer <habibaomer675@gmail.com>
2024-07-13 16:29:16 +0300

    Add rectangle function

--- AreaAndPerimeter/AreaAndPerimeter/Program.cs
+++ AreaAndPerimeter/AreaAndPerimeter/Program.cs
@@ -8,40 +8,20 @@ namespace AreaAndPerimeter
 {
     class Program
     {
-        //1
-        public static void Square()
+        //3
+        public static void Rectangle()
         {
-            Console.WriteLine("Enter the sidelength of the square: ");
-            double sidelength = double.Parse(Console.ReadLine());
-            double area = sidelength * sidelength;
-            double perimeter = 4 * sidelength;
-            Console.WriteLine($"The Area Of Square = {area} And The perimeter = {perimeter}.");
-        }
-        //2
-        public static void Triangle()
-        {
-            Console.WriteLine("The Area Of Triangle: ");
-            Console.WriteLine("Enter the baselength of the triangle: ");
-            double baselength = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the heigth of the triangle: ");
-            double heigth = double.Parse(Console.ReadLine());
-            double area = 0.5 * baselength * heigth;
-            Console.WriteLine($"The Area Of Triangle = {area}.");
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("The perimeter Of Triangle: ");
-            Console.WriteLine("Enter the length of the first side of the triangle: ");
-            double side1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the length of the secound side of the triangle: ");
-            double side2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the length of the third side of the triangle: ");
-            double side3 = double.Parse(Console.ReadLine());
-            double perimeter = side1 + side2 + side3;
-            Console.WriteLine($"The perimeter Of Triangle = {perimeter}.");
+            Console.WriteLine("Enter the length of the square: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width of the square: ");
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            double perimeter = 2 * (length * width);
+            Console.WriteLine($"The Area Of Rectangle = {area} And The perimeter = {perimeter}.");
         }
         static void Main(string[] args)
         {
-            Square();
-            Triangle();
+            Rectangle();
             Console.ReadLine();
         }
     }
